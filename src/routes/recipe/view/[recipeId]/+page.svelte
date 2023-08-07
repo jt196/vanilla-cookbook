890f4a3142e94d830c4d3aa9d5a5b702cfaee530
jt196 <jamestorr@gmail.com>
2023-08-07 15:56:00 +0100

    bold ingredients #16

--- src/routes/recipe/view/[recipeId]/+page.svelte
+++ src/routes/recipe/view/[recipeId]/+page.svelte
@@ -17,53 +17,74 @@
 	let ingredientsArray: IParsedIngredient[] = []
 	let scale = 1
 	let scaledServings: string | null
+	let directionLines: string[] = []
 
 	$: if (data && data.recipe) {
 		recipe = data.recipe
 		ingredients = recipe.ingredients ? recipe.ingredients.split('\n') : []
 		ingredientsArray = ingredientProcess(ingredients)
+		recipe.directions ? (directionLines = recipe.directions.split('\n')) : null
 		scaledServings = recipe.servings ? scaleNumbersInString(recipe.servings, scale) : null
 	}
 </script>
 
 <h3>{recipe?.name}</h3>
 
-<div class="recipe-thumbnail">
-	{#if recipe?.image_url && startsWithHttp(recipe.image_url)}
-		<img src={recipe.image_url} alt="{recipe.name} thumbnail" />
-	{:else}
-		<FoodBowl height="400px" />
-	{/if}
+<div class="grid">
+	<div>
+		<div class="recipe-thumbnail">
+			{#if recipe?.image_url && startsWithHttp(recipe.image_url)}
+				<img src={recipe.image_url} alt="{recipe.name} thumbnail" />
+			{:else}
+				<FoodBowl height="400px" />
+			{/if}
+		</div>
+
+		<p>Created: <i>{localDateAndTime(recipe.created)}</i></p>
+		<p>
+			Source:
+			<a href={recipe?.source_url}>{recipe?.source}</a>
+		</p>
+		{#if ingredients}
+			<p>
+				{scaledServings}
+			</p>
+			<p>
+				Scale
+				<Scale bind:scale />
+			</p>
+		{/if}
+	</div>
+	<div>
+		<p>Ingredients:</p>
+		<ul>
+			{#each ingredientsArray as ingredient}
+				<li>
+					<strong>
+						{ingredient.quantity ? decimalToFraction(ingredient.quantity * scale) : ''}
+					</strong>
+					{ingredient.unit && ingredient.unit !== 'q.b.' ? ingredient.unit : ''}
+					{ingredient.ingredient}
+				</li>
+			{/each}
+		</ul>
+	</div>
 </div>
 
-<p>
-	Description:
-	{recipe?.description}
-</p>
-<p>Created: <i>{localDateAndTime(recipe.created)}</i></p>
-<p>
-	Source:
-	<a href={recipe?.source_url}>{recipe?.source}</a>
-</p>
-{#if ingredients}
+{#if recipe?.description}
+	<h4>Description:</h4>
 	<p>
-		{scaledServings}
-	</p>
-	<p>
-		Scale
-		<Scale bind:scale />
+		{recipe?.description}
 	</p>
 {/if}
-<p>Ingredients:</p>
-<ul>
-	{#each ingredientsArray as ingredient}
-		<li>
-			{ingredient.quantity ? decimalToFraction(ingredient.quantity * scale) : ''}
-			{ingredient.unit && ingredient.unit !== 'q.b.' ? ingredient.unit : ''}
-			{ingredient.ingredient}
-		</li>
+
+{#if directionLines}
+	<h4>Directions:</h4>
+	{#each directionLines as line}
+		<p>{line}</p>
 	{/each}
-</ul>
+{/if}
+
 <a href="/recipe/edit/{recipe?.uid}" role="button" class="outline contrast">Edit</a>
 
 <style lang="scss">
