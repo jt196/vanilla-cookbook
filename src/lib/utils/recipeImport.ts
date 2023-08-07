6c195baf4e9f1dc53a97fe854948920e645cb723
jt196 <jamestorr@gmail.com>
2023-08-07 11:54:13 +0100

    Refactoring seed.ts file #19

---
+++ src/lib/utils/recipeImport.ts
@@ -0,0 +1,92 @@
+import PrismaClientPkg from '@prisma/client'
+import { promises as fsPromises } from 'fs'
+import fs from 'fs'
+import type { PaprikaRecipe, Category } from '../types'
+
+// // Prisma doesn't support ES Modules so we have to do this
+const PrismaClient = PrismaClientPkg.PrismaClient
+// Different name of prismaC as the auth is importing prisma from the adapter
+const prismaC = new PrismaClient()
+
+export async function loadPaprikaRecipes(): Promise<PaprikaRecipe[]> {
+	const filePath = 'src/lib/data/recipes-small.json'
+
+	if (!fs.existsSync(filePath)) {
+		console.log('File does not exist:', filePath)
+		return []
+	}
+
+	try {
+		const jsonString = await fsPromises.readFile(filePath, 'utf8')
+		return JSON.parse(jsonString)
+	} catch (err) {
+		console.log('An error occurred:', err)
+		return []
+	}
+}
+
+export async function importPaprikaCategories(recipes: PaprikaRecipe[]) {
+	// Collecting all unique categories
+	const categoriesMap: { [key: string]: Category } = {}
+
+	recipes.forEach((recipe) => {
+		recipe.categories
+			? recipe.categories.forEach((category) => {
+					categoriesMap[category.uid] = category as Category
+			  })
+			: undefined
+	})
+
+	// Gather all unique parent UIDs
+	const parentUids: Set<string> = new Set()
+
+	Object.values(categoriesMap).forEach((category) => {
+		if (category.parent_uid) {
+			parentUids.add(category.parent_uid)
+		}
+	})
+
+	// Create parent categories if they don't exist
+	for (const parentUid of parentUids) {
+		const existingCategory = await prismaC.category.findUnique({
+			where: { uid: parentUid }
+		})
+
+		if (!existingCategory) {
+			await prismaC.category.create({
+				data: { uid: parentUid } // Only the UID is needed
+			})
+		}
+	}
+
+	// Creating or updating categories, connecting to parents if applicable
+	for (const category of Object.values(categoriesMap)) {
+		const existingCategory = await prismaC.category.findUnique({
+			where: { uid: category.uid }
+		})
+
+		const categoryData = {
+			uid: category.uid,
+			order_flag: category.order_flag,
+			name: category.name,
+			parent: category.parent_uid
+				? {
+						connect: { uid: category.parent_uid }
+				  }
+				: undefined
+		}
+
+		if (existingCategory) {
+			// Update if it exists
+			await prismaC.category.update({
+				where: { uid: category.uid },
+				data: categoryData
+			})
+		} else {
+			// Create if it doesn't exist
+			await prismaC.category.create({
+				data: categoryData
+			})
+		}
+	}
+}
