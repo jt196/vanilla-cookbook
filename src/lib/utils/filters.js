f10b36d5d3a15fe3ac220561ed0a18579457ddd0
jt196 <jamestorr@gmail.com>
2023-08-12 15:52:56 +0100

    Fractions bug fixes #28

--- src/lib/utils/filters.js
+++ src/lib/utils/filters.js
@@ -57,6 +57,33 @@ export function ingredientProcess(ingredientArray) {
 export function sanitizeForYamlFrontMatter(str) {
 	if (!str) return ''
 
+	const fractionMap = {
+		'½': '1/2',
+		'⅓': '1/3',
+		'⅔': '2/3',
+		'¼': '1/4',
+		'¾': '3/4',
+		'⅕': '1/5',
+		'⅖': '2/5',
+		'⅗': '3/5',
+		'⅘': '4/5',
+		'⅙': '1/6',
+		'⅚': '5/6',
+		'⅐': '1/7',
+		'⅛': '1/8',
+		'⅜': '3/8',
+		'⅝': '5/8',
+		'⅞': '7/8',
+		'⅑': '1/9',
+		'⅒': '1/10'
+		// Add more fractions if needed
+	}
+
+	for (let [fraction, ascii] of Object.entries(fractionMap)) {
+		const regex = new RegExp(fraction, 'g')
+		str = str.replace(regex, ascii)
+	}
+
 	str = str
 		.split('')
 		.filter((char) => {
