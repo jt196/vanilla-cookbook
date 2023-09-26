97386248e648364fcea0009ed02f514f91aaf2b6
James Torr <mail@beercat.co.uk>
2023-09-26 09:30:27 +0100

    Fixing deployment issues #73

--- src/lib/server/lucia.js
+++ src/lib/server/lucia.js
@@ -2,18 +2,24 @@ import lucia from 'lucia-auth'
 import "lucia-auth/polyfill/node";
 import prismaAdapter from '@lucia-auth/adapter-prisma'
 import { sveltekit } from 'lucia-auth/middleware'
-import { dev } from '$app/environment'
 import { prisma } from '$lib/server/prisma'
+
+const isDev = process.env.VITE_ENV === 'development';
 
 export const auth = lucia({
 	adapter: prismaAdapter(prisma),
-	env: dev ? 'DEV' : 'PROD',
+	env: isDev ? 'DEV' : 'PROD',
 	middleware: sveltekit(),
 	transformDatabaseUser: (userData) => {
 		return {
 			userId: userData.id,
 			username: userData.username,
 			name: userData.name
+		}
+	},
+	sessionCookie: {
+		attributes: {
+			sameSite: 'strict'
 		}
 	}
 })
