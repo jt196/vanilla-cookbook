fab37fbfe9c49a7c6e2dca41f2c2a6bc953662a2
jt196 <jamestorr@gmail.com>
2023-10-14 14:17:33 +0100

    Adding ingredients seed data #151

--- prisma/seed.js
+++ prisma/seed.js
@@ -6,6 +6,8 @@ import { sveltekit } from 'lucia-auth/middleware'
 import prisma from '@lucia-auth/adapter-prisma'
 import { promises as fsPromises } from 'fs'
 import { config } from 'dotenv'
+import fs from 'fs'
+import csv from 'csv-parser'
 
 config()
 
@@ -69,6 +71,70 @@ function getUsers() {
 	]
 }
 
+async function seedIngredients() {
+	try {
+		// Check if the CSV file exists
+		const fileExists = await fs.promises
+			.access('./src/lib/data/ingredients/dry_ingredient_data.csv')
+			.then(() => true)
+			.catch(() => false)
+
+		if (!fileExists) {
+			console.log('CSV file does not exist, cannot continue seeding.')
+			return
+		}
+
+		// Check the current version from SiteSettings
+		const siteSettings = await prismaC.siteSettings.findFirst()
+		const currentVersion = siteSettings?.version || 0
+
+		// Define the expected version
+		const expectedVersion = 1 // Change this as needed
+
+		// Proceed with seeding if currentVersion is less than expectedVersion
+		if (currentVersion < expectedVersion) {
+			const data = []
+
+			// Clear the existing data in the Ingredient table
+			await prismaC.ingredient.deleteMany()
+
+			// Read the CSV file using csv-parser with proper configuration
+			fs.createReadStream('./src/lib/data/ingredients/dry_ingredient_data.csv')
+				.pipe(csv({ separator: ',' })) // Specify the separator as a comma
+				.on('data', async (row) => {
+					const names = row.names
+					const gramsPerCup = parseFloat(row.gramsPerCup)
+
+					// Insert data into the Ingredient table with the full names
+					data.push(
+						prismaC.ingredient.create({
+							data: {
+								name: names,
+								gramsPerCup: gramsPerCup
+							}
+						})
+					)
+				})
+				.on('end', async () => {
+					// Wait for all data insertions to complete
+					await Promise.all(data)
+
+					// Update the version in SiteSettings
+					await prismaC.siteSettings.update({
+						where: { id: siteSettings?.id || 1 },
+						data: { version: expectedVersion }
+					})
+
+					console.log('Data has been seeded.')
+				})
+		} else {
+			console.log('Data is up to date. No need to seed.')
+		}
+	} catch (error) {
+		console.error('Error seeding data:', error)
+	}
+}
+
 // 5. Get Admin User ID
 async function getRootUserId() {
 	const rootUser = await prismaC.authUser.findFirst({
@@ -106,10 +172,12 @@ async function seed() {
 			})
 		}
 
+		await seedIngredients()
+
 		// Try to get the admin user from the db
 		let rootUserId = await getRootUserId()
 		if (rootUserId) {
-			console.log('Database is already seeded.')
+			console.log('Database is already seeded with root user.')
 			await prismaC.$disconnect()
 			return
 		}
