f32f5888b9493ad3f8459a48ea0e6a0fcd3813e1
jt196 <jamestorr@gmail.com>
2024-02-26 10:27:46 +0000

    Using reactive statement client side #203

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,12 @@
 <script>
-	import { onMount } from 'svelte'
 	import { goto } from '$app/navigation'
 
 	export let data
 	const { user } = data
 
-	onMount(() => {
-		if (user && user.userId) {
-			goto(`/user/${user.userId}/recipes`) // Redirect to the recipe page if the user is logged in
-		}
-	})
+	$: if (user && user.userId) {
+		goto(`/user/${user.userId}/recipes`)
+	}
 </script>
 
 <h1>Cookrr Personal Cookbook</h1>
