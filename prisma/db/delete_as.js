bdba43fc016d04607746ab07aa462674a1858f61
jt196 <jamestorr@gmail.com>
2025-03-04 09:23:34 +0000

    commit before upgrade

--- prisma/db/delete_as.js
+++ prisma/db/delete_as.js
@@ -1,4 +1,8 @@
 import PrismaClientPkg from '@prisma/client'
+import { lucia } from 'lucia'
+import { sveltekit } from 'lucia/middleware'
+import { prisma } from '@lucia-auth/adapter-prisma'
+import 'lucia/polyfill/node'
 
 // // Prisma doesn't support ES Modules so we have to do this
 const PrismaClient = PrismaClientPkg.PrismaClient
@@ -14,15 +18,37 @@ const prismaC = new PrismaClient({
 	}
 })
 
+// Have to invoke the auth module here as it's outside the project directory.
+export const auth = lucia({
+	adapter: prisma(new PrismaClient(), {
+		user: 'authUser',
+		key: 'authKey',
+		session: 'authSession'
+	}),
+	env: 'DEV',
+	middleware: sveltekit()
+})
+
+// Delete a specific session
+async function deleteSession(id) {
+	await prismaC.authSession.delete({
+		where: {
+			id: id
+		}
+	})
+}
+
+// Delete all auth sessions then disconnect from the database
 async function deleteAndDisconnect() {
 	await prismaC.authSession.deleteMany()
 	await prismaC.$disconnect()
 }
 
+// Reset the user password in case of lock out
+async function updatePassword() {
+	await auth.updateKeyPassword('username', 'jt196', 'homersdad')
+}
+
 deleteAndDisconnect()
 
-// await prismaC.authSession.delete({
-//   where: {
-//     id: "6lZxnZROnBWOqvNKIs0vIzrKtQ3yD7dtdMMbKLaK"
-//   }
-// });
+// updatePassword();
