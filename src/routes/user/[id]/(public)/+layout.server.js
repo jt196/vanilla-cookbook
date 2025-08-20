d2b8af7e40f648c9dc04eeb1c145cba4d78afaa1
jt196 <jamestorr@gmail.com>
2025-08-20 16:41:16 +0100

    redirect bug fix. Yuck, how did I miss that.

--- src/routes/user/[id]/(public)/+layout.server.js
+++ src/routes/user/[id]/(public)/+layout.server.js
@@ -1,3 +1,5 @@
+import { redirect } from '@sveltejs/kit'
+
 /**
  * Server-side logic to load recipes for the page.
  * @returns {Promise<Object>} An object containing the recipes ordered by their creation date in descending order.
